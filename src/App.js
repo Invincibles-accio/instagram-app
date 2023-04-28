98d5bc52d4677c590ea53f82b1fb5180aeca686c
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-28 12:17:56 +0530

    test connection with firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase,ref, set  } from "firebase/database";
 import firebaseApp from "./firebase";
 
 
-
+const database = getDatabase(firebaseApp);
 const App = () => {
-  const database = getDatabase(firebaseApp);
-
+  
+    function putData(){
+        set( ref(database, "users/abhishek"),
+         {
+              id: 1,
+              name: "Abhishek Choudhary",
+              age: 21,
+              email: "abhi@a.com"
+         }
+        )
+    }
 
   return(
     <div>
          <h1>Instagram</h1>
+         <button onClick={putData}>Add Data</button>
     </div>
   )
 }
+
+
+export default App
